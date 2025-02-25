cb8ca0bd380e466c5efbf391b56bd0bdfc23f220
Thomas <thomasantana_@hotmail.com>
2025-02-25 20:58:53 -0300

    até o método soma não estático

--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -1,8 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Aula02._1bim;
 using System.Xml.Serialization;
 
-int num1 = 20;
-int num2 = 2;
-int soma = num1 + num2;
+//int num1 = 20;
+//int num2 = 2;
+//int soma = num1 + num2;
 
-Console.WriteLine($"O resultado foi {soma}");
+//Console.WriteLine($"O resultado foi {soma}");
+
+Console.WriteLine("informe sua idade: ");
+int idade;
+idade = int.Parse(Console.ReadLine());
+Console.WriteLine("Minha idade:" + idade);
+
+int valor1 = 10;
+valor1 += 20; //valor1 = valor1 + 20
+
+SomaDoisNumeros somador;
+somador = new SomaDoisNumeros();
+
+Console.WriteLine("Resultado :" + somador.Somar(20, 22));
