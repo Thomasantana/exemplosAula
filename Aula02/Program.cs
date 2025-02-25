e3507fdf39fdd6d3ed63b6801907bb72921d6414
2023122310 <2023122310@LAB1-014>
2025-02-25 19:29:59 -0300

    Adicionar arquivos de projeto.

---
+++ Aula02/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Serialization;
+
+int num1 = 20;
+int num2 = 2;
+int soma = num1 + num2;
+
+Console.WriteLine($"O resultado foi {soma}");
